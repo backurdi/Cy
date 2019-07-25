584aed74f5aeb8a691fbfeee8e7e0a29d6528e33
Ghazwan Cheikh-Youssef <ghazwancheikh-youssef@Ghazwans-iMac.local>
2019-07-25 09:28:07 +0200

    initial start

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import '../css/style.scss';
+import { nav } from '../reusable/reusable';
+
+nav('.nav', '/nav.html')
+console.log("Hello World from forside main file!");
---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+/*
+ * We've enabled HtmlWebpackPlugin for you! This generates a html
+ * page for you when you compile webpack, which will make you start
+ * developing and prototyping faster.
+ *
+ * https://github.com/jantimon/html-webpack-plugin
+ *
+ */
+
+module.exports = {
+	mode: 'development',
+
+	entry: {
+		babel: 'babel-polyfill',
+		index: './src/js/index.js',
+	},
+
+	output: {
+		filename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/html/index.html',
+			chunks: ['index']
+		}),
+		new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].sass",
+            chunkFilename: "[id].sass"
+        }),
+	],
+
+	module: {
+		rules: [
+			{
+				test: /.(js|jsx)$/,
+				include: [path.resolve(__dirname, 'src')],
+				loader: 'babel-loader',
+
+				options: {
+					plugins: ['syntax-dynamic-import'],
+
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								modules: false
+							}
+						]
+					]
+				}
+			},
+			{
+                test: /\.(s*)css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                  'file-loader',
+                  {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      disable: true, // webpack@2.x and newer
+                    },
+                  },
+                ],
+              }
+		]
+	},
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					priority: -10,
+					test: /[\\/]node_modules[\\/]/
+				}
+			},
+
+			chunks: 'async',
+			minChunks: 1,
+			minSize: 30000,
+			name: true
+		}
+	},
+
+	devServer: {
+		open: true
+	}
+};
