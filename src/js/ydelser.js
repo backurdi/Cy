dfc4b851dbff09840e8f0770aabb59abe14c3b5f
bachir <bachir098@gmail.com>
2019-09-02 12:09:19 +0200

    ydelser structure done

--- src/js/ydelser.js
+++ src/js/ydelser.js
@@ -1,9 +1,20 @@
 import '../css/ydelser.scss';
-import {
-    insertElement
-} from '../reusable/reusable';
+import { insertElement } from '../reusable/reusable';
+import $ from 'jquery';
 
-const memberImagePath = require.context('../img', true);
+const memberImagePath = require.context(
+  '../img',
+  true,
+);
 
 insertElement('nav', 'nav.html');
 insertElement('footer', 'footer.html');
+
+const navElement = $(
+  '.toggle-nav ul li',
+);
+
+console.log(navElement);
+navElement.click(el => {
+    console.log(el.find('a'));
+});
